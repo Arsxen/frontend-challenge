173db7efb46538305632fd34fecaba6af4a21e5f
Arsxen <archawat9@gmail.com>
2025-02-16 14:36:07 +0700

    setup repository

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-const nextJest = require('next/jest');
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   dir: './',
-});
+})
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
@@ -15,6 +15,6 @@ const customJestConfig = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: ['/node_modules/'],
-};
+}
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig)
--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import Index from '../app/index';
+import { render, screen } from '@testing-library/react'
+import Index from '../app/index'
 
 describe('Home', () => {
   it('renders without errors', () => {
-    const { container } = render(<Index />);
-    expect(container).toBeDefined();
-  });
-});
+    const { container } = render(<Index />)
+    expect(container).toBeDefined()
+  })
+})
